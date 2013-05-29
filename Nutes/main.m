d02158e0d157d3e3ba6964365f08f88c5eff15eb
andrewmelis <amelis@pathf.com>
2013-05-29 11:06:04 -0500

    Initial Commit

---
+++ Nutes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nutes
+//
+//  Created by Andrew Melis on 5/29/13.
+//  Copyright (c) 2013 Baller Status Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
